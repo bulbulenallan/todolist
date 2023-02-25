128a8224f9a107c4e13e0d41b975a8f88f05b241
allan kipchumba <bulbulen5606@gmail.com>
2023-02-25 13:52:31 +0300

    main

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1> HELLO MY NAME IS BULBULEN ALLAN</h1>
+    <h2>AND THIS IS MY FIRST REACT APP</h2>
+      
+    </div>
+  );
+}
+
+export default App;
